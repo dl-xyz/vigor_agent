c1633e77ecdc4e8bd2c2ad8bf1bc333ea1ddb9f7
perpetualCreations <tchen0584@gmail.com>
2022-06-20 19:33:02 -0400

    Authentication backend.

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,24 @@
 
 //! # Vigor
 //! This library contains a Vigor authentication agent to manage credentials and perform HTTP/HTTPS requests.
+//!
+//! A note regarding Ed25519: this client library supports Ed25519 authentication, however will only accept PEM-encoded keys.
+//! Formats such as OpenSSH are not guaranteed to work.
+//! The private key is expected to adhere to RFC 7468, PKCS8 and unencrypted.
+//!
 
 use std::fs;
 use std::fmt;
-use dirs::home_dir;
 use std::path::PathBuf;
-use serde::{Serialize, Deserialize};
+
+extern crate dirs;
+extern crate serde;
+extern crate reqwest;
+extern crate pem_rfc7468;
+extern crate ed25519_dalek;
+extern crate hex;
+use dirs::home_dir;
+use ed25519_dalek::Signer;
 
 /// This library's vender-specific error type.
 ///
@@ -29,14 +41,14 @@ impl fmt::Display for Error {
 }
 
 // definitions for configuration structs.
-#[derive(Serialize, Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 struct ConfigEd25519Schema {
     public: String,
     private: String,
     enabled: bool
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize)]
 struct ConfigSchema {
     preferred_username: String,
     email: String,
@@ -44,12 +56,19 @@ struct ConfigSchema {
     ed25519: ConfigEd25519Schema
 }
 
+#[derive(serde::Serialize, serde::Deserialize)]
+struct Authentication {
+    mode: String,
+    answer: String
+}
+
 /// Represents configuration and path information for agent structure.
 ///
 /// Consume implemented methods for initialization, see `new` method.
 pub struct Vigor {
     config: ConfigSchema,
-    path: PathBuf
+    path: PathBuf,
+    client: reqwest::blocking::Client
 }
 
 impl fmt::Debug for Vigor {
@@ -150,29 +169,193 @@ impl Vigor {
             Ok(config_path) => {
                 Ok(Vigor {
                     config: ConfigSchema {
-                        preferred_username: String::from("nobody"),
-                        email: String::from("nobody@localhost"),
-                        password: String::from("hunter2"), // i'm not funny.
+                        preferred_username: "nobody".to_owned(),
+                        email: "nobody@localhost".to_owned(),
+                        password: "hunter2".to_owned(), // i'm not funny.
                         ed25519: ConfigEd25519Schema {
-                            public: String::from("/path/to/your/keys/vigor.pem.pub"),
-                            private: String::from("/path/to/your/keys/vigor.pem"),
+                            public: "/path/to/your/keys/vigor.pem.pub".to_owned(),
+                            private: "/path/to/your/keys/vigor.pem".to_owned(),
                             enabled: false
                         }
                     },
-                    path: config_path
+                    path: config_path,
+                    client: reqwest::blocking::Client::new()
                 })
             },
             Err(error) => Err(error)
         }
     }
+
+    fn host_finalize(&self, host: &str) -> String {
+        let mut url = PathBuf::from(host);
+        url.push(&self.config.preferred_username);
+        url.display().to_string()
+    }
+
+    /// Performs account creation to a Vigor host.
+    ///
+    /// This method expects three booleans after the host argument for whether email, password, and/or Ed25519 should be shared.
+    /// At least one authentication method must be shared.
+    ///
+    /// # Examples
+    ///
+    /// ```ignore
+    /// // assuming you already have an instance called "agent"
+    /// agent.put("http://example.com/claims/", true, true, true).unwrap();
+    /// ```
+    pub fn put(&self, host: &str, share_email: bool, use_password: bool, use_ed25519: bool) -> Result<(), Error> {
+        let mut payload = serde_json::Map::new();
+        if !use_password && !use_ed25519 {
+            return Err(Error {message: "At least one authentication method must exist on the new account.".to_owned()})
+        }
+        if share_email {
+            payload.insert("email".to_owned(), serde_json::Value::String(self.config.email.to_owned()));
+        }
+        if use_password {
+            match Vigor::get_authentication_password(self) {
+                Ok(password) => {
+                    payload.insert("password".to_owned(), serde_json::Value::String(password));
+                },
+                Err(error) => {
+                    return Err(error);
+                }
+            }
+        }
+        if use_ed25519 {
+            match fs::read_to_string(&self.config.ed25519.public) {
+                Ok(data) => {
+                    payload.insert("ed25519key".to_owned(), serde_json::Value::String(data));
+                }
+                Err(error) => {
+                    return Err(Error {message: error.to_string()})
+                }
+            }
+        }
+        match self.client.put(Vigor::host_finalize(self, &host)).json(&serde_json::to_string(&payload).unwrap()).send() {
+            Ok(_) => Ok(()),
+            Err(error) => {
+                Err(Error {message: format!("{}", error)})
+            }
+        }
+    }
+
+    fn get_authentication_ed25519(&self) -> Result<String, Error> {
+        match fs::read_to_string(&self.config.ed25519.private) {
+            Ok(data) => {
+                match pem_rfc7468::decode_vec(data.as_bytes()) {
+                    Ok(data) => {
+                        let raw = data.1;
+                        if raw.len() < 32  {
+                            return Err(Error {message: "Ed25519 private key is not at least 32 bytes.".to_owned()});
+                        }
+                        let key_as_bytes = &raw[(raw.len() - 32)..]; // drop excess bytes (i.e. ID bytes)
+                        match ed25519_dalek::SecretKey::from_bytes(&key_as_bytes) {
+                            Ok(secret_key) => {
+                                let public_key: ed25519_dalek::PublicKey = (&secret_key).into();
+                                let keypair = ed25519_dalek::Keypair {public: public_key, secret: secret_key};
+                                match keypair.try_sign("SIGNME".as_bytes()) {
+                                    Ok(signature) => {
+                                        Ok(hex::encode(signature.to_bytes()))
+                                    },
+                                    Err(error) => Err(Error {message: error.to_string()})
+                                }
+                            },
+                            Err(error) => Err(Error {message: error.to_string()})
+                        }
+                    },
+                    Err(error) => Err(Error {message: error.to_string()})
+                }
+            }
+            Err(error) => Err(Error {message: error.to_string()})
+        }
+    }
+
+    fn get_authentication_password(&self) -> Result<String, Error> {
+        if self.config.password.is_empty() {
+            return Err(Error {message: "Password cannot be of zero length.".to_owned()})
+        } else {
+            return Ok(self.config.password.to_owned())
+        }
+    }
+
+    fn form_authentication_ed25519(&self) -> Result<String, Error> {
+        match Vigor::get_authentication_ed25519(self) {
+            Ok(answer) => {
+                let json = Authentication {
+                    mode: "ed25519".to_owned(),
+                    answer: answer
+                };
+                Ok(serde_json::to_string(&json).unwrap())
+            },
+            Err(error) => Err(error)
+        }
+    }
+
+    fn form_authentication_password(&self) -> Result<String, Error> {
+        match Vigor::get_authentication_password(self) {
+            Ok(answer) => {
+                let json = Authentication {
+                    mode: "password".to_owned(),
+                    answer: answer
+                };
+                Ok(serde_json::to_string(&json).unwrap())
+            },
+            Err(error) => Err(error)
+        }
+    }
+
+    fn form_authentication(&self, mode: AuthMode) -> Result<String, Error> {
+        match mode {
+            AuthMode::Ed25519 => {
+                Vigor::form_authentication_ed25519(self)
+            },
+            AuthMode::Password => {
+                Vigor::form_authentication_password(self)
+            },
+            AuthMode::Auto => {
+                if self.config.ed25519.enabled {
+                    match Vigor::form_authentication_ed25519(self) {
+                        Ok(payload) => {
+                            return Ok(payload)
+                        },
+                        Err(_) => {}
+                    };
+                }
+                match Vigor::form_authentication_password(self) {
+                    Ok(payload) => {
+                        return Ok(payload)
+                    },
+                    Err(_) => {
+                        return Err(Error {message: "No authentication modes available that aren't disabled or erroneous.".to_owned()});
+                    }
+                };
+            }
+        }
+    }
+
+    /// Performs account deletion to a Vigor host.
+    ///
+    /// # Examples
+    ///
+    /// ```ignore
+    /// // assuming you already have an instance called "agent"
+    /// agent.delete("http://example.com/claims/").unwrap();
+    /// ```
+    pub fn delete(&self, host: &str, mode: AuthMode) -> Result<(), Error> {
+        match Vigor::form_authentication(self, mode) {
+            Ok(payload) => {
+                match self.client.delete(Vigor::host_finalize(self, &host)).json(&payload).send() { // TODO payload fix.
+                    Ok(_) => Ok(()),
+                    Err(error) => Err(Error {message: format!("{}", error)})
+                }
+            },
+            Err(error) => Err(error)
+        }
+    }
+
+    // function here to patch
+
+    // function here to get token.
+
+    // function here to perform request with token automatically as header.
 }
-
-// function here to put
-
-// function here to delete
-
-// function here to patch
-
-// function here to get token.
-
-// function here to perform request with token automatically as header.
